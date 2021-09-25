28f600580df72f159ea10379f308ff6152336e5c
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2021-09-25 12:47:07 -0300

    Initial Commit

---
+++ dataStructures-002-lista_simplesmente/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  dataStructures-002-lista_simplesmente
+//
+//  Created by Luiz Carlos da Silva Araujo on 25/09/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
